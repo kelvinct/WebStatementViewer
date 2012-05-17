efa5502b1506414c40c228cecffc9bae8a756982
kelvinct <kelvinct@yahoo.com.hk>
2012-05-17 16:00:42 +0800

    test

---
+++ WebStatementViewerC/default.aspx.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+namespace WebStatementViewerC
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        SqlConnection ImpdbConn = new SqlConnection(ConfigurationManager.ConnectionStrings["ImpdbConn"].ConnectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            DateTime current;
+            string result;
+            DropDownList1.DataTextFormatString = "{0:dd/MM/yyyy}";
+            if (IsPostBack == false)
+            {
+                dateoffTrade();
+                current = Convert.ToDateTime(DropDownList1.SelectedValue);
+                result = current.ToString("yyyyMMdd");
+                uptolist(result);
+                Label1.Text = current.ToString("yyyy/MM/dd");
+            }
+        }
+
+        private void dateoffTrade()
+        {
+          //  SqlConnection ImpdbConn = new SqlConnection(ConfigurationManager.ConnectionStrings["ImpdbConn"].ConnectionString);
+            string buildSQL = "SELECT (CONVERT(datetime,( CONVERT(varchar(8), UploadTime, 112)),106 ) ) as UploadTime  FROM ProcessQry where datediff(day , getdate() , uploadtime ) > -10 group by CONVERT(varchar(8), UploadTime, 112) Order By CONVERT(varchar(8), UploadTime, 112) DESC";
+            SqlCommand cmd = new SqlCommand(buildSQL, ImpdbConn);
+            cmd.Connection.Open();
+            SqlDataReader ddlValues;
+            ddlValues = cmd.ExecuteReader();
+            DropDownList1.DataSource = ddlValues;
+            DropDownList1.DataValueField = "UploadTime";
+            DropDownList1.DataTextField = "UploadTime";
+            DropDownList1.DataBind();
+            cmd.Connection.Close();
+          //  cmd.Connection.Dispose();
+
+
+
+
+        }
+        private void uptolist(string getdate)
+        {
+         //   SqlConnection ImpdbConn = new SqlConnection(ConfigurationManager.ConnectionStrings["ImpdbConn"].ConnectionString);
+            getdate = getdate.Substring(2, 6);
+          //  DateTime current;
+            string buildSQL = "SELECT UploadTime,ClientCode,Status,StartTime,EndTime FROM ProcessQry   where UploadFileDate ='" + (getdate) + "' and UploadTime > '16:00' Order By UploadTime";
+            SqlCommand cmd = new SqlCommand(buildSQL, ImpdbConn);
+            cmd.Connection.Open();
+            SqlDataReader ddlValues;
+            ddlValues = cmd.ExecuteReader();
+            GridView1.DataSource = ddlValues;
+            GridView1.DataBind();
+            cmd.Connection.Close();
+           // cmd.Connection.Dispose();
+
+
+        }
+
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DateTime current;
+            string result;
+
+            current = Convert.ToDateTime(DropDownList1.SelectedValue);
+            result = current.ToString("yyyyMMdd");
+            uptolist(result);
+        }
+
+        protected void RowDatebound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+
+                string Cell = e.Row.Cells[2].Text;
+                if (Cell == "S")
+                {
+                    e.Row.Cells[2].Text = "Success";
+                }
+                else if (Cell == "F")
+                {
+                    e.Row.Cells[2].Text = "Fail";
+                }
+                else if (Cell == "P")
+                {
+                    e.Row.Cells[2].Text = "Pending";
+                }
+                else if (string.IsNullOrEmpty(Cell)== true)
+                {
+                    e.Row.Cells[2].Text = "Processing";
+                }
+            }
+        }
+
+    }
+
+
+}
---
+++ WebStatementViewerC/obj/Debug/Package/PackageTmp/default.aspx
@@ -0,0 +1,68 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="WebStatementViewerC._default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+
+    
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <link href="style.css" rel="stylesheet" type="text/css" />
+    <style type="text/css">
+        .style1
+        {
+            width: 215px;
+            height: 215px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div align="center">
+        <font face="arial"><b><small>WebStatement Import Viewer
+        <asp:Label ID="Label1" runat="server"></asp:Label>
+        </small></b></font>
+    </div>
+    <hr />
+    <div id="dropdownlist" runat="server">
+        <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True" 
+            DataFormatString="{0:dd/MM/yyyy}" Width="100%" 
+            onselectedindexchanged="DropDownList1_SelectedIndexChanged">
+        </asp:DropDownList>
+    </div>
+    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" 
+        BackColor="White" BorderColor="#DEDFDE" BorderStyle="None" BorderWidth="1px" 
+        CellPadding="4" Font-Names="Arial" Font-Size="Smaller" ForeColor="Black" 
+        GridLines="Vertical" RowHeaderColumn="1" Width="100%" 
+        onselectedindexchanged="GridView1_SelectedIndexChanged" 
+        onrowdatabound="RowDatebound">
+        <AlternatingRowStyle BackColor="White" CssClass="table1" />
+        <Columns>
+            <asp:BoundField DataField="UploadTime" DataFormatString="{0:dd/MM/yyyy}" 
+                HeaderText="UploadTime" ReadOnly="True" SortExpression="UploadTime" />
+            <asp:BoundField DataField="ClientCode" HeaderText="ClientCode" ReadOnly="True" 
+                SortExpression="ClientCode" />
+            <asp:BoundField DataField="Status" HeaderText="Status" 
+                SortExpression="Status" />
+            <asp:BoundField DataField="StartTime" DataFormatString="{0:dd/MM/yyyy}" 
+                HeaderText="StartTime" SortExpression="StartTime" />
+            <asp:BoundField DataField="EndTime" DataFormatString="{0:dd/MM/yyyy}" 
+                HeaderText="EndTime" SortExpression="EndTime" />
+        </Columns>
+        <EditRowStyle Font-Size="Small" />
+        <FooterStyle BackColor="#CCFFCC" />
+        <HeaderStyle BackColor="#6B696B" Font-Bold="True" Font-Names="Arial" 
+            Font-Size="X-Small" ForeColor="White" />
+        <PagerStyle BackColor="#66FFFF" ForeColor="Black" HorizontalAlign="Right" />
+        <RowStyle BackColor="#CCFFFF" />
+       
+                        </asp:GridView>
+    </form>
+    <hr />
+    <div align="center">
+        CamsView 2.0</div>
+
+        
+</body>
+</html>
